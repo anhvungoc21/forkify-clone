69be8ebafe90692e4758bbb0363ad807a026373f
Anh Vu <anhvungoc.21@gmail.com>
2022-05-17 06:17:24 -0500

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https:forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RESULTS_PER_PAGE = 10;
 export const APIKEY = 'fc27ef0f-91db-425c-9844-70a0d09dca97';
